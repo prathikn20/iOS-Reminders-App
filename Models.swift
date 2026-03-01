155d44f2482c6ceda0e38b5edb20efcd180d6adb
prathikn20 <prathikn@ad.unc.edu>
2026-03-01 17:55:14 -0500

    Add Reminder and RemindersPage structures

---
+++ Models.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct Reminder: Identifiable {
+    var id: UUID = UUID()
+    var title: String
+    var isCompleted: Bool = false
+}
+
+struct RemindersPage: Identifiable {
+    var id: UUID = UUID()
+    var title: String
+    var items: [Reminder]
+    var color: Color
+}
